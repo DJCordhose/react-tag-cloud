a65d3fde72ed208a83c1457691af7ad1414ec8d4
Oliver Zeigermann <oliver@zeigermann.de>
2024-01-21 21:07:06 +0100

    better modal

--- src/DescriptionModal.js
+++ src/DescriptionModal.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Modal from 'react-modal';
+import './DescriptionModal.css'; // Import the CSS file
 
 Modal.setAppElement('#root'); // This binds your modal to your application root
 
 const DescriptionModal = ({ isOpen, content, onRequestClose }) => (
-  <Modal isOpen={isOpen} onRequestClose={onRequestClose} contentLabel="Description">
+  <Modal 
+    isOpen={isOpen} 
+    onRequestClose={onRequestClose} 
+    contentLabel="Description"
+    className="modal-content"
+    overlayClassName="modal-background"
+  >
     <div>
       {typeof content === 'string' ? <p>{content}</p> : <img src={content} alt="Description" />}
       <button onClick={onRequestClose}>Close</button>
